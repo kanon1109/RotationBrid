3a7c038673d4d42c9ba515b971df9332d3a9e22a
kanontang <kanontang@gmail.com>
2014-06-18 23:02:16 +0800

    增加小鸟速度数据。

--- Classes/data/BirdVo.cpp
+++ Classes/data/BirdVo.cpp
@@ -1,10 +1,12 @@
 #include "BirdVo.h"
 
-
 BirdVo::BirdVo(void)
 {
+	this->angleSpeed = 0;
+	this->angleSpeedAdd = 1;
+	this->vx = 0;
+	this->vy = 0;
 }
-
 
 BirdVo::~BirdVo(void)
 {
@@ -12,6 +14,7 @@ BirdVo::~BirdVo(void)
 
 void BirdVo::update()
 {
+	this->angleSpeed += this->angleSpeedAdd;
 	this->x += this->vx;
 	this->y += this->vy;
 }
--- Classes/data/BirdVo.h
+++ Classes/data/BirdVo.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef _BIRD_VO_
+#define _BIRD_VO_
 class BirdVo
 {
 public:
@@ -13,5 +15,10 @@ private:
 	//坐标
 	float x;
 	float y;
+	//角速度
+	float angleSpeed;
+	//角速度加值
+	float angleSpeedAdd;
 };
 
+#endif // !1
