e7f710b673e6568ec98fa5ab022317e59ef8cbe4
kanontang <kanontang@gmail.com>
2014-06-30 22:53:50 +0800

    修改获取鸟的头尾坐标方法。

--- Classes/GameStage.cpp
+++ Classes/GameStage.cpp
@@ -363,9 +363,11 @@ void GameStage::checkThough()
 	Node* container = (Node*)gameLayer->getChildByTag(birdContainerTag);
 	//更新小鸟的位置和角度
 	Sprite* birdSpt = (Sprite*)container->getChildByTag(birdTag);
-	
-	float bx = birdSpt->getContentSize().width * .5 * cos(bird::MathUtil::dgs2rds(birdSpt->getRotation()));
-	float by = birdSpt->getContentSize().width * .5 * sin(bird::MathUtil::dgs2rds(birdSpt->getRotation()));
+	float birdAngle = birdSpt->getRotation();
+	float birdRad = bird::MathUtil::dgs2rds(birdAngle);
+	float dis = birdSpt->getContentSize().width * .5;
+	float bx = dis * cos(birdRad);
+	float by = dis * sin(birdRad);
 	debugNode->clear();
 	//头部坐标
 	Vec2 headBirdPos = Vec2(birdSpt->getPositionX() + bx, 
@@ -396,7 +398,6 @@ void GameStage::checkThough()
 			Point p = Point(x, y);
 			p = wall->getParent()->convertToWorldSpace(p);
 			debugNode->drawDot(p, 5, ColorUtil::getColor4F(0xFF, 0x00, 0x00, 0xFF));
-			//CCLOG("%f %f", pos.x, pos.y);
 		}
 	}
 }
